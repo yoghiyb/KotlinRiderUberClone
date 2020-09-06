84fe74ffb31fc0f6f770656c25b38880a03553d6
yoghiyb <yoghiyb@gmail.com>
2020-09-06 19:38:51 +0700

    Display Infor and Sign Out

--- app/src/main/java/com/yoghi/kotlinrideruberclone/HomeActivity.kt
+++ app/src/main/java/com/yoghi/kotlinrideruberclone/HomeActivity.kt
@@ -1,7 +1,12 @@
 package com.yoghi.kotlinrideruberclone
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,10 +18,18 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.yoghi.kotlinrideruberclone.common.Common
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+    private lateinit var img_avatar: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +38,9 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        drawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        navView = findViewById(R.id.nav_view) as NavigationView
+        navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -37,6 +50,45 @@ class HomeActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        init()
+    }
+
+    private fun init() {
+        navView.setNavigationItemSelectedListener {
+            if (it.itemId == R.id.nav_sign_out) {
+                val builder = AlertDialog.Builder(this@HomeActivity)
+                builder.setTitle("Sign out")
+                    .setMessage("Do you really want to sign out?")
+                    .setNegativeButton("CANCEL") { dialogInterface, _ -> dialogInterface.dismiss() }
+                    .setPositiveButton("SIGN OUT") { dialogInterface, _ ->
+                        FirebaseAuth.getInstance().signOut()
+                        val intent =
+                            Intent(this@HomeActivity, SplashScreenActivity::class.java)
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        startActivity(intent)
+                        finish()
+                    }.setCancelable(false)
+
+                val dialog = builder.create()
+                dialog.setOnShowListener {
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                        .setTextColor(ContextCompat.getColor(this@HomeActivity, android.R.color.holo_red_dark))
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                        .setTextColor(ContextCompat.getColor(this@HomeActivity, android.R.color.holo_red_dark))
+                }
+
+                dialog.show()
+            }
+            true
+        }
+
+        val headerView = navView.getHeaderView(0)
+        val txt_name = headerView.findViewById<View>(R.id.txt_name) as TextView
+        val txt_phone = headerView.findViewById<View>(R.id.txt_phone) as TextView
+
+        img_avatar = headerView.findViewById<View>(R.id.img_avatar) as ImageView
+        txt_name.setText(Common.buildWelcomeMessage())
+        txt_phone.setText(Common.currentRider!!.phoneNumber)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/yoghi/kotlinrideruberclone/common/Common.kt
+++ app/src/main/java/com/yoghi/kotlinrideruberclone/common/Common.kt
@@ -4,6 +4,14 @@ import com.yoghi.kotlinrideruberclone.model.RiderModel
 
 object Common {
 
+    fun buildWelcomeMessage(): String {
+        return StringBuilder("Welcome, ")
+            .append(currentRider!!.firstName)
+            .append(" ")
+            .append(currentRider!!.lastName)
+            .toString()
+    }
+
     var currentRider: RiderModel? = null
     val RIDER_INFO_REFERENCE: String = "Riders"
 }
